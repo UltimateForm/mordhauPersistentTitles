6a60c29f812a05ab6f3cf76a4418804dc5f01cab
ultimateform <heliojmsemedo@gmail.com>
2024-05-30 00:12:02 +0100

    alternative to pipenv

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import asyncio
 import json
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 from data import Config, load_config, save_config
 from rcon_listener import RconListener
 from login_observer import LoginObserver
 
+load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
 
